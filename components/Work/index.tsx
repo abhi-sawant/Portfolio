0a3795365a01e648f316847cc4bbf07721274100
Abhishek <abhisawant111111@gmail.com>
2023-04-07 07:41:45 +0530

    Internal links

--- components/Work/index.tsx
+++ components/Work/index.tsx
@@ -4,7 +4,7 @@ import { workdata } from './workdata';
 
 function Work() {
   return (
-    <section className='py-8 lg:py-16'>
+    <section className='py-8 lg:py-16' id='work'>
       <div className='container mx-auto px-4'>
         <SectionTitle>Work</SectionTitle>
         <div className='sm grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-12'>
